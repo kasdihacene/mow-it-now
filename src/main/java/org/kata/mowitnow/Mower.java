b87ebc9d3ac582cbc67597ef1c978304d1c8aa73
Hacene KASDI <hacene.upmc@gmail.com>
2021-12-28 11:44:01 +0100

    [MowItNow] (TDD Loop) - Mower logic change orientation according to Commands D-G

--- src/main/java/org/kata/mowitnow/Mower.java
+++ src/main/java/org/kata/mowitnow/Mower.java
@@ -11,8 +11,29 @@ public class Mower {
 
     String position;
     String movement;
+    private static final String MOVEMENT_DG = "NESW"; // North Est West South
+
+    public static char nextOrientation(String command, String currentOrientation) {
+        int indexOfCurrentOrientation = MOVEMENT_DG.indexOf(currentOrientation);
+
+        if (command.equals("D")) {
+            return (indexOfCurrentOrientation + 1 == MOVEMENT_DG.length()) ?
+                    MOVEMENT_DG.charAt(0) : MOVEMENT_DG.charAt(indexOfCurrentOrientation + 1);
+
+        }
+        return (indexOfCurrentOrientation - 1 < 0) ?
+                MOVEMENT_DG.charAt(MOVEMENT_DG.length() - 1) : MOVEMENT_DG.charAt(indexOfCurrentOrientation - 1);
+    }
 
     public void move(String lawnBorders) {
-
+        String[] commands = movement.split("");
+        for (String command : commands) {
+            switch (command) {
+                case "D":
+                case "G":
+                case "A":
+                default:
+            }
+        }
     }
 }
--- src/test/java/org/kata/mowitnow/MowerTest.java
+++ src/test/java/org/kata/mowitnow/MowerTest.java
@@ -23,4 +23,56 @@ class MowerTest {
         // Then
         assertThat(mower.getPosition()).isEqualTo("5 1 E");
     }
+
+    @Test
+    void should_move_right_to_South_orientation_when_having_current_orientation_Est() {
+        // Given
+        String commandMove = "D";
+        String currentOrientation = "E";
+
+        // When
+        char orientation = Mower.nextOrientation(commandMove, currentOrientation);
+
+        // Then
+        assertThat(orientation).isEqualTo('S');
+    }
+
+    @Test
+    void should_move_right_to_West_orientation_when_having_current_orientation_South() {
+        // Given
+        String commandMove = "D";
+        String currentOrientation = "S";
+
+        // When
+        char orientation = Mower.nextOrientation(commandMove, currentOrientation);
+
+        // Then
+        assertThat(orientation).isEqualTo('W');
+    }
+
+    @Test
+    void should_move_right_to_North_orientation_when_having_current_orientation_West() {
+        // Given
+        String commandMove = "D";
+        String currentOrientation = "W";
+
+        // When
+        char orientation = Mower.nextOrientation(commandMove, currentOrientation);
+
+        // Then
+        assertThat(orientation).isEqualTo('N');
+    }
+
+    @Test
+    void should_move_right_to_Est_orientation_when_having_current_orientation_North() {
+        // Given
+        String commandMove = "D";
+        String currentOrientation = "N";
+
+        // When
+        char orientation = Mower.nextOrientation(commandMove, currentOrientation);
+
+        // Then
+        assertThat(orientation).isEqualTo('E');
+    }
 }
